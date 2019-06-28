bcbc10e3aafd9793dbfc087d8a5ccd5d00690746
Sanjeev Tamang <sanzeevtamang@gmail.com>
2019-06-28 11:05:09 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class dog
+  def initialize(name,bread)
+    @name = name
+    @bread = name
+  end 
+  
+  def name
+    @name
+  end 
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def breed 
+    @breed 
+  end 
+  
+  def breed=(breed="Mutt")
+    @breed = breed 
+  end 
+  
+end 
